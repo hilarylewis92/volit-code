4b16ef3c8e633b3323872fdf5694540c4fd481db
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-16 09:49:48 -0700

    View form when logging in

--- src/views/Main/Login.js
+++ src/views/Main/Login.js
@@ -15,8 +15,20 @@ export class Login extends React.Component {
     const { auth } = this.props
     return (
       <div>
-        <h2>Please Login</h2>
-        <button onClick={auth.login.bind(this)}>Login</button>
+        <form className='login-form'>
+          <header>
+            <h2>Welcome to Volit!</h2>
+            <p>The simplest way to schedule your team of volunteers</p>
+          </header>
+          <section>
+            <p>Enter your email and organization name to create an account</p>
+            <label htmlFor='email'>Your email</label>
+            <input type='text' id='email' placeholder='example@domain.com'/>
+            <label htmlFor='organization'>Organization Name</label>
+            <input type='text' id='organization' placeholder='Habitat for Humanity'/>
+          </section>
+        </form>
+        <button onClick={auth.login.bind(this)}>Create Account</button>
       </div>
     )
   }
