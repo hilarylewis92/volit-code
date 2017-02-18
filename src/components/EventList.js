58fbe6ff330751c36599828653e97548dd44e662
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-18 10:58:40 -0700

    Set up event list component

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,9 +3,36 @@ import React, { PropTypes as T } from 'react'
 export class EventList extends React.Component {
 
   render(){
+    var eventMonthCount=5
+    var eventYearCount=35
     return (
-      <div>
-        Event list
+      <div className='EventList'>
+        <header
+          className='event-list-header'>
+          <h3
+            className='event-list-title'>
+            Upcoming Events
+          </h3>
+          <button
+            className='event-list-new-event-btn'>
+            New Event
+          </button>
+          <span
+            className='event-list-count'>
+            {eventMonthCount} events this month,
+            {eventYearCount} events this year
+          </span>
+        </header>
+
+        <ul
+          className='event-list-date'>
+          <span>date of event</span>
+          <li
+            className='event-list-list-item'>
+            <h5>title of event</h5>
+            <p>description of event</p>
+          </li>
+        </ul>
       </div>
     )
   }
