555dfcdc69af36db985ba51828a74654725f79d3
Nick <nicknchambers@gmail.com>
2017-02-22 14:18:20 -0700

    deploys DB to production

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.ELEPHANTSQL_URL,
     migrations: {
       directory: './db/migrations'
     },
