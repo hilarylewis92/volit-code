20d897c8607edcb7901e3d47f7772250349ca47b
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-27 16:47:50 -0700

    [TESTS]: ADMIN_LOGIN and CREATE_EVENT actions

--- test/actions/index.spec.js
+++ test/actions/index.spec.js
@@ -1,64 +1,80 @@
 import * as actions from '../../src/actions/index.js';
 import * as types from '../../src/actions/ActionTypes.js';
 
-describe('actions', () => {
+describe('ADMIN_LOGIN actions', () => {
+  let res, expected;
 
-  it('the setProfile action creator should create an ADMIN_login action', () => {
-    const res = {organization: [{admin_id: 1, id: 1, name: 'Habitat for Alex'}], user: {email:'lol@yahoo.com', id: 1, name: "Tofu Salad", phone_number: '555-555-6666'}}
+  beforeEach(() => {
+    res = {
+      organization: [{
+        admin_id: 1,
+        id: 1,
+        name: 'Habitat for Alex' }],
+      user: {
+        email:'lol@yahoo.com',
+        id: 1,
+        name: "Tofu Salad",
+        phone_number: '555-555-6666'
+      }}
 
-    const expected = {
+    expected = {
       type: types.ADMIN_LOGIN,
-      organization: {admin_id: 1, id: 1, name: 'Habitat for Alex'},
-      user: {email:'lol@yahoo.com', id: 1, name: "Tofu Salad", phone_number: '555-555-6666'}
+      organization: { admin_id: 1, id: 1, name: 'Habitat for Alex' },
+      user: { email:'lol@yahoo.com', id: 1, name: "Tofu Salad", phone_number: '555-555-6666' }
     }
+  })
 
+  it('the setProfile action creator should create action object', () => {
     expect(actions.setProfile(res)).toEqual(expected)
+  })
 
-  });
+  it('should have an organization property', () => {
+    expect(actions.setProfile(res).organization).toEqual(expected.organization)
+  })
 
-  it('the setEvents action create should create a CREATE_EVENT action', () => {
-    const res = {
-      event_address:"1052 Chowda street", event_date:"2017-02-22T07:00:00.000Z", event_description:"It's going to be great", event_name:"Foam Partay",id: 1, organization_id: 1, volunteer_count: null
+  it('should have an user property', () => {
+    expect(actions.setProfile(res).user).toEqual(expected.user)
+  })
+})
+
+describe('CREATE_EVENT actions', () => {
+  let res, expected;
+
+  beforeEach(() => {
+    res = {
+      event_address:"1052 Chowda street", event_date:"2017-02-22T07:00:00.000Z",
+      event_description:"It's going to be great",
+      event_name:"Foam Partay",
+      id: 1,
+      organization_id: 1,
+      volunteer_count: null
     }
-    const expected = {
+
+    expected = {
       type: types.CREATE_EVENT,
       res: {
-        event_address:"1052 Chowda street", event_date:"2017-02-22T07:00:00.000Z", event_description:"It's going to be great", event_name:"Foam Partay",id: 1, organization_id: 1, volunteer_count: null
+        event_address:"1052 Chowda street", event_date:"2017-02-22T07:00:00.000Z",
+        event_description:"It's going to be great",
+        event_name:"Foam Partay",id: 1,
+        organization_id: 1,
+        volunteer_count: null
       }
     }
+  })
 
+  it('should have an event_name, vent_address, event_date, event_address property', () => {
+    expect(actions.setEvents(res).event_name).toEqual(expected.event_name)
+    expect(actions.setEvents(res).event_address).toEqual(expected.event_address)
+    expect(actions.setEvents(res).event_date).toEqual(expected.event_date)
+    expect(actions.setEvents(res).event_address).toEqual(expected.event_address)
+  })
+
+  it('should have an organization_id and volunteer_count property', () => {
+    expect(actions.setEvents(res).organization_id).toEqual(expected.organization_id)
+    expect(actions.setEvents(res).volunteer_count).toEqual(expected.volunteer_count)
+  })
+
+  it('the setEvents action create should create an action object', () => {
     expect(actions.setEvents(res)).toEqual(expected)
-  });
-
-  // xit('should create an action to add an item', () => {
-  //   const item = 'example two';
-  //   const expectedAction = {
-  //     type: types.ITEM__CREATE,
-  //     item
-  //   };
-  //
-  //   expect(actions.addItemSuccess(item)).toEqual(expectedAction);
-  // });
-  //
-  // xit('should create an action to add a flash message', () => {
-  //   const text = 'A thing was successful!';
-  //   const messageType = 'notification';
-  //   const expectedAction = {
-  //     type: types.FLASH_MESSAGE__CREATE,
-  //     messageType,
-  //     text
-  //   };
-  //
-  //   expect(actions.addFlashMessage(text, messageType)).toEqual(expectedAction);
-  // });
-  //
-  // xit('should create an action to delete a flash message', () => {
-  //   const timestamp = 12345;
-  //   const expectedAction = {
-  //     type: types.FLASH_MESSAGE__DELETE,
-  //     timestamp
-  //   };
-  //
-  //   expect(actions.deleteFlashMessage(timestamp)).toEqual(expectedAction);
-  // });
-});
+  })
+})
