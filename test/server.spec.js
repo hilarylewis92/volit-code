f5179d8c045d1364fd8ed6ab4695ba72f6fcc952
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-28 15:13:43 -0700

    Pass all tests for routes

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,6 +1,5 @@
 const expect = require('chai').expect
 const should = require('chai').should
-const jasmine = require('jasmine')
 const request = require('supertest')
 const app = require('../server/server.js')
 
@@ -12,9 +11,30 @@ describe('server tests', () => {
     done()
   })
 
+  it('should get /api/org_check/:name', (done) => {
+    request(app)
+    .get('/api/org_check/habitatforhumanity')
+    .expect(200)
+    done()
+  })
+
   it('should post /api/organizations', (done) => {
     request(app)
     .post('/api/organizations')
+    .expect(200)
+    done()
+  })
+
+  it('should post /api/organizations/:admin_id', (done) => {
+    request(app)
+    .post('/api/organizations/1')
+    .expect(200)
+    done()
+  })
+
+  it('should get /api/user/:email/:org_name', (done) => {
+    request(app)
+    .get('/api/user/hilarylewis92@gmail.com/habitatforhumanity')
     .expect(200)
     done()
   })
@@ -32,10 +52,66 @@ describe('server tests', () => {
     .expect(200)
     done()
   })
+
+  it('should get /api/events/:organization_id', (done) => {
+    request(app)
+    .get('/api/events/1')
+    .expect(200)
+    done()
+  })
+
+  it('should post /api/events/:organization_id', (done) => {
+    request(app)
+    .post('/api/events/1')
+    .expect(200)
+    done()
+  })
+
+  it('should put /api/events/:organization_id', (done) => {
+    request(app)
+    .put('/api/events/1')
+    .expect(200)
+    done()
+  })
+
+  it('should delete /api/events/:organization_id/:id', (done) => {
+    request(app)
+    .delete('/api/events/1/1')
+    .expect(200)
+    done()
+  })
+
+  it('should get /api/roles/:event_id', (done) => {
+    request(app)
+    .get('/api/roles/1')
+    .expect(200)
+    done()
+  })
+
+  it('should post /api/roles/:event_id', (done) => {
+    request(app)
+    .post('/api/roles/1')
+    .expect(200)
+    done()
+  })
+
+  it('should patch /api/roles/:event_id/:id', (done) => {
+    request(app)
+    .patch('/api/roles/1/1')
+    .expect(200)
+    done()
+  })
+
+  it('should delete /api/roles/:event_id/:id', (done) => {
+    request(app)
+    .delete('/api/roles/1/1')
+    .expect(200)
+    done()
+  })
 })
 
 describe('undefined routes', function(){
-  it('should post /api/users', (done) => {
+  it('should return 404', (done) => {
     request(app)
     .post('/api/not-real')
     .expect(404)
