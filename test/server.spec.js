a05b27a9eed9f7604fe443eb7395c25cf8d286f1
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-28 15:02:40 -0700

    Tests pass for all non-dynamic routes

--- test/server.spec.js
+++ test/server.spec.js
@@ -2,13 +2,43 @@ const expect = require('chai').expect
 const should = require('chai').should
 const jasmine = require('jasmine')
 const request = require('supertest')
-const app = require('../server/server.js');
+const app = require('../server/server.js')
 
 describe('server tests', () => {
-  xit('/api/organizations', (done) => {
+  it('should get /api/organizations', (done) => {
     request(app)
     .get('/api/organizations')
     .expect(200)
     done()
-  });
-});
+  })
+
+  it('should post /api/organizations', (done) => {
+    request(app)
+    .post('/api/organizations')
+    .expect(200)
+    done()
+  })
+
+  it('should get /api/users', (done) => {
+    request(app)
+    .get('/api/users')
+    .expect(200)
+    done()
+  })
+
+  it('should post /api/users', (done) => {
+    request(app)
+    .post('/api/users')
+    .expect(200)
+    done()
+  })
+})
+
+describe('undefined routes', function(){
+  it('should post /api/users', (done) => {
+    request(app)
+    .post('/api/not-real')
+    .expect(404)
+    done()
+  })
+})
